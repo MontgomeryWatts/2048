9b2074b691d6820e06848c0629226475a1eff638
Montgomery <montgomerywatts@yahoo.com>
2019-02-17 23:46:23 -0500

    Grouped code with similar functions. Added a lot of comments. All display-related constants are derived from three main constants, which can be altered to change the appearance of the game.

--- src/maingame.py
+++ src/maingame.py
@@ -6,18 +6,22 @@ from pygame.locals import *
 
 pygame.init()
 
-BOARD_SIZE = 4
-WINDOW_DIMENSION = 500
-BOARD = Board(BOARD_SIZE)
+# Control Variables: All other constants depend on these
+BOARD_SIZE = 4  # How many cells are in the n x n board
+WINDOW_DIMENSION = 500  # The square dimensions of the window
 BOARD_RATIO = 0.8  # How much of the window the board takes up
-BOARD_OFFSET = WINDOW_DIMENSION * (1 - BOARD_RATIO) / 2
+
+BOARD = Board(BOARD_SIZE)
+OFFSET_RATIO = ((1 - BOARD_RATIO) / 2)
+BOARD_OFFSET = WINDOW_DIMENSION * OFFSET_RATIO
 BOARD_DIMENSION = WINDOW_DIMENSION * BOARD_RATIO
-BORDER_DIMENSION = BOARD_DIMENSION + 2
+BORDER_DIMENSION = BOARD_DIMENSION + 2  # 1 pixel border
 BORDER_OFFSET = BOARD_OFFSET - 1
 DISPLAY_SURF = pygame.display.set_mode((WINDOW_DIMENSION, WINDOW_DIMENSION))
 BORDER_RECT = (BORDER_OFFSET, BORDER_OFFSET, BORDER_DIMENSION, BORDER_DIMENSION)
 BOARD_RECT = (BOARD_OFFSET, BOARD_OFFSET, BOARD_DIMENSION, BOARD_DIMENSION)
-FONT = pygame.font.SysFont("Arial", 12)
+CELL_DIMENSION = BOARD_DIMENSION / BOARD_SIZE
+FONT = pygame.font.SysFont("Arial", int(CELL_DIMENSION * OFFSET_RATIO * 2))
 
 LIGHT_GRAY = (225, 225, 225)
 GRAY = (169,169,169)
@@ -29,15 +33,14 @@ PASTEL_ORANGE = (255, 223, 186)
 PASTEL_YELLOW = (255, 255, 186)
 PASTEL_GREEN = (186, 255, 201)
 PASTEL_BLUE = (186, 225, 255)
-PASTLE_PURPLE = (149, 125, 173)
+PASTEL_PURPLE = (149, 125, 173)
 LIME = (153, 255, 153)
 CYAN = (153, 204, 255)
 PERIWINKLE = (204, 153, 255)
 
-DISPLAY_SURF.fill(WHITE)
-
 pygame.display.set_caption('2048')
-pygame.draw.rect(DISPLAY_SURF, BLACK, BORDER_RECT)
+DISPLAY_SURF.fill(WHITE)
+pygame.draw.rect(DISPLAY_SURF, BLACK, BORDER_RECT)  # Draw game border and board
 pygame.draw.rect(DISPLAY_SURF, GRAY, BOARD_RECT)
 
 
@@ -57,25 +60,23 @@ def get_color(cell_value):
 	elif cell_value == 128:
 		return PASTEL_BLUE
 	elif cell_value == 256:
-		return PASTLE_PURPLE
+		return PASTEL_PURPLE
 	elif cell_value == 512:
 		return LIME
 	elif cell_value == 1024:
 		return CYAN
 	elif cell_value == 2048:
 		return PERIWINKLE
-	else:
+	else:  # Placeholder for now, granted I do not allow players to continue after reaching 2048
 		return WHITE
 
 
 def draw_board():
-	cell_dimension = BOARD_DIMENSION / BOARD_SIZE
-
 	for row in range(BOARD_SIZE):
-		cell_top_offset = BOARD_OFFSET + ((row + 0.1) * cell_dimension)
+		cell_top_offset = BOARD_OFFSET + ((row + OFFSET_RATIO) * CELL_DIMENSION)
 		for col in range(BOARD_SIZE):
-			cell_left_offset = BOARD_OFFSET + ((col + 0.1) * cell_dimension)
-			cell_rect = (cell_left_offset,  cell_top_offset, cell_dimension * .8, cell_dimension * .8)
+			cell_left_offset = BOARD_OFFSET + ((col + OFFSET_RATIO) * CELL_DIMENSION)
+			cell_rect = (cell_left_offset,  cell_top_offset, CELL_DIMENSION * BOARD_RATIO, CELL_DIMENSION * BOARD_RATIO)
 
 			cell = BOARD.get_cell_at(row, col)
 			if cell.value is 0:
@@ -84,12 +85,14 @@ def draw_board():
 				color = get_color(cell.value)
 				pygame.draw.rect(DISPLAY_SURF, color, cell_rect)
 				text = FONT.render(cell.__str__(), True, BLACK)
+
+				# Roughly centers the text of the cell's value
 				DISPLAY_SURF.blit(text, (cell_rect[0] + (cell_rect[2] / 2), cell_rect[1] + (cell_rect[2] / 2)))
 
 
 draw_board()
 
-while BOARD.game_not_over(): # main game loop
+while BOARD.game_not_over():  # main game loop
 
 	for event in pygame.event.get():
 		if event.type == KEYUP:
